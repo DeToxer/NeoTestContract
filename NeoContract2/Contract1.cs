1ba08980564cd9b3c74d1e4abd41c88699533f47
Detoxer <m_pettersen91@hotmail.com>
2017-09-18 22:10:41 +0200

    Add project files.

---
+++ NeoContract2/Contract1.cs
@@ -0,0 +1,26 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+using System.Numerics;
+
+namespace NeoContract2
+{
+    public class Contract1 : SmartContract
+    {
+        public static String Main(String a, String b){
+            if (a.Equals("is dank"))
+            {
+                return "Dank memes";
+            } else
+            {
+                return "Deep fried memes";
+            }
+  
+        }
+
+        public static int Sum (int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
